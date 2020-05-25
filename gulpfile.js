ba1dd151f5d87a766491a8a9959d1c56b8bf1830
Ivan Tishkin <Ivan1969@211.ru>
2020-05-26 03:59:47 +0500

    gulp 4
    gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,128 +1,150 @@
-var gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
-    browserSync  = require('browser-sync'),
-    concat       = require('gulp-concat'),
-    concatCss    = require('gulp-concat-css');
-    uglify       = require('gulp-uglifyjs'),
-    cssnano      = require('gulp-cssnano'),
-    rename       = require('gulp-rename'),
-    del          = require('del'),
-    imagemin     = require('gulp-imagemin'),
-    jpgmin       = require('imagemin-jpegoptim'),
-    pngmin       = require('imagemin-pngquant'),
-    cache        = require('gulp-cache'),
-    autoprefixer = require('gulp-autoprefixer'),
-    gih          = require("gulp-include-html"),
-    deploy       = require('gulp-gh-pages');
+const gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  browsersync = require('browser-sync').create(),
+  concat = require('gulp-concat'),
+  concatCss = require('gulp-concat-css'),
+  uglify = require('gulp-uglifyjs'),
+  cssnano = require('gulp-cssnano'),
+  rename = require('gulp-rename'),
+  del = require('del'),
+  imagemin = require('gulp-imagemin'),
+  jpgmin = require('imagemin-jpegoptim'),
+  pngmin = require('imagemin-pngquant'),
+  cache = require('gulp-cache'),
+  autoprefixer = require('gulp-autoprefixer'),
+  gih = require("gulp-include-html");
 
-gulp.task('sass', function(){
-  return gulp.src('app/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer(['last 15 versions', '> 1%'], { cascade: true }))
+const sassInit = () => {
+  return gulp
+    .src('app/sass/**/*.scss')
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(autoprefixer(['last 15 versions', '> 1%']))
     .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({stream: true}))
-});
+};
 
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: 'app'
-        },
-        notify: false
-    });
-});
+const browserSyncReload = (done) => {
+  browsersync.reload();
+  done();
+}
 
-gulp.task('scripts', function() {
-    return gulp.src([
-      'app/libs/jquery/dist/jquery.min.js',
-      'PATH_TO_ANOTHER_SCRIPTS'
+const browserSync = (done) => {
+  browsersync.init({
+    server: {
+      baseDir: "app"
+    },
+    port: 3000,
+    notify: false
+  });
+  done();
+}
+
+const scripts = () => {
+  return (
+    gulp
+    .src([
+      'PATH_TO_JS_LIBS'
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('app/js'));
-});
+    .pipe(gulp.dest('app/js'))
+  );
+}
 
-gulp.task('css-libs', function() {
-    return gulp.src([
-      'PATH_TO_CSS_LIB',
-      'PATH_TO_CSS_LIB'
+const css = () => {
+  return gulp
+    .src([
+      'PATH_TO_CSS_LIBS'
     ])
     .pipe(concatCss('libs.min.css'))
     .pipe(cssnano())
     .pipe(gulp.dest('app/css'));
-});
+}
 
-gulp.task('build-html' , function(){
-  return gulp.src("app/html/pages/*.html")
-  .pipe(gih({
-    baseDir:'app/html/'
-  }))
-  .pipe(gulp.dest("app"));
-});
+const includeHtml = () => {
+  return gulp
+    .src("app/html/pages/*.html")
+    .pipe(gih({
+      baseDir: 'app/html/'
+    }))
+    .pipe(gulp.dest("app"));
+}
 
-gulp.task('watch', ['browser-sync', 'css-libs', 'scripts', 'build-html'], function() {
-    gulp.watch('app/sass/**/*.scss', ['sass']);
-    gulp.watch('app/html/**/*.html', ['build-html']);
-    gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/js/**/*.js', browserSync.reload);
-});
+const watchFiles = () => {
+  gulp.watch("app/sass/**/*.scss", gulp.series(sassInit, browserSyncReload));
+  gulp.watch("app/js/**/*", gulp.series(browserSyncReload));
+  gulp.watch("app/html/**/*.html", gulp.series(includeHtml, browserSyncReload));
+  gulp.watch('app/*.html', gulp.series(browserSyncReload));
+  gulp.watch("app/images/**/*", images);
+}
 
-gulp.task('clean', function() {
-    return del.sync('dist');
-});
+const deleteDist = (done) => {
+  del.sync(['dist']);
+  done();
+};
 
-gulp.task('img', function() {
-  return gulp.src('app/images/**/*')
-  .pipe(cache(imagemin([
-    imagemin.gifsicle({interlaced: true}),
-    jpgmin({
-      progressive: true,
-      max: 85,
-      stripAll: true
-    }),
-    pngmin({
-      quality: '68'
-    }),
-    imagemin.svgo({
-      plugins: [
-        {removeViewBox: true},
-        {cleanupIDs: false}
-      ]
-    })
-  ])))
+const images = () => {
+  return (
+    gulp
+    .src('app/images/**/*')
+    .pipe(imagemin([
+      imagemin.gifsicle({
+        interlaced: true
+      }),
+      jpgmin({
+        progressive: true,
+        max: 90,
+        stripAll: true
+      }),
+      pngmin({
+        quality: [0.7, 0.9]
+      }),
+      imagemin.svgo({
+        plugins: [{
+            removeViewBox: true
+          },
+          {
+            cleanupIDs: false
+          }
+        ]
+      })
+    ]))
+    .pipe(gulp.dest('dist/images'))
+  );
+}
 
-  .pipe(gulp.dest('dist/images'));
-});
+const clear = () => {
+  return cache.clearAll();
+}
 
-gulp.task('clear', function () {
-    return cache.clearAll();
-});
-
-
-gulp.task('deploy', function () {
-  return gulp.src("./dist/**/*")
-    .pipe(deploy({
-      remoteUrl: "URL_TO_YOUR.git",
-      branch: "master"
-    }));
-});
-
-
-// Huyak huyak and to production
-gulp.task('build', ['clean', 'clear', 'img', 'sass', 'scripts', 'css-libs'], function() {
-
-    var buildCss = gulp.src('app/css/**/*')
+const moveAll = (done) => {
+  const buildCss = gulp.src('app/css/**/*')
     .pipe(gulp.dest('dist/css'))
 
-    var buildFonts = gulp.src('app/fonts/**/*')
+  const buildFonts = gulp.src('app/fonts/**/*')
     .pipe(gulp.dest('dist/fonts'))
 
-    var buildJs = gulp.src('app/js/**/*')
+  const buildJs = gulp.src('app/js/**/*')
     .pipe(gulp.dest('dist/js'))
 
-    var buildHtml = gulp.src('app/*.html')
+  const buildHtml = gulp.src('app/*.html')
     .pipe(gulp.dest('dist'));
 
-});
+  done();
+}
 
-gulp.task('default', ['watch']);
+const js = gulp.series(scripts);
+const build = gulp.series(deleteDist, clear, sassInit, css, images, js, moveAll);
+const watch = gulp.parallel(watchFiles, browserSync);
+
+exports.images = images;
+exports.css = css;
+exports.sassInit = sassInit;
+exports.js = js;
+exports.deleteDist = deleteDist;
+exports.clear = clear;
+exports.build = build;
+exports.watch = watch;
+
+exports.default = watch;
